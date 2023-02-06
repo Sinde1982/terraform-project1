2483d22817fb94ad86643e11a2dd2dcf92dda1de
Sinde1982 <122070677+Sinde1982@users.noreply.github.com>
2023-02-06 13:55:31 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA4QWKOSZ7BVC76L5B"
+  secret_key = "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"
+}
+
+resource "aws_instance" "one" {
+  ami = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.micro"
+  key_name = "project"
+  availability_zone = "us-east-1"
+  tags = {
+    Name = "thiru-tf"
+  }
+} 
